0586f7bd8d9ae7a949fe09a1f5d47c2e07b38723
thutran <TTran@securitybank.com.ph>
2025-03-25 22:07:53 +0700

    implement

--- cypress/e2e/bdd/test-report.cy.js
+++ cypress/e2e/bdd/test-report.cy.js
@@ -3,6 +3,7 @@ describe("Sample Report Test", () => {
       cy.visit("https://example.cypress.io");
       cy.contains("type").click();
       cy.url().should("include", "/commands/actions");
+
     });
   });
 
